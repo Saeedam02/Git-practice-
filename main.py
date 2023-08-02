cc008a1ff0374a0a278795b2ca9e8da7370a3987
Saeed Aghamohammadi <saeed008190@gmail.com>
2023-08-02 10:22:01 +0330

    adding streamlit

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 print('hello word')
 
-<<<<<<< HEAD
 #this is a test
-=======
 def add(a,b):
     s=a+b
     return s
@@ -12,4 +10,3 @@ def mul(e,r):
     w=e+r
     return w
 mul(3,8)
->>>>>>> testing
---
+++ src/run.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title('paytopia dashboard')
+st.text('Hi')
+
+st.code('x=2 , y=5 , x+y')
