4cebeabb5a4e8be33a347dc0f261a25a8720a28a
Saeed Aghamohammadi <saeed008190@gmail.com>
2023-08-03 20:53:58 +0330

    updating the run.py

--- src/run.py
+++ src/run.py
@@ -2,13 +2,25 @@ import streamlit as st
 import pandas as pd
 from io import StringIO
 import json
-
+import seaborn as sb
+import numpy as np
+import matplotlib.pyplot as plt
 st.title(':zap: paytopia dashboard')
 st.text('Hi')
 
 st.code('x=2 , y=5 ,')
 
+try:
+    from stringI0 import stringI0 ##for python 2
+except ImportError:
+    from io import StringIO ## for python 3
 
+    with st.expander('ststistics'):
+        fig,ax =plt.subplots(1,1, figsize=(10,5))
+        sb.histplot(np.random.randn(100),ax=ax)
+        st.pyplot(fig)
+
+# st.camera_input('camera input',key='camera_input')
 
 # uploaded_file = st.file_uploader("Choose a file")
 # if uploaded_file is not None:
